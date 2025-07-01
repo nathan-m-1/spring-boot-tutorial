952f3cee30474cad3737d66e0584e78c53180669
nathan <nathan@cognitiveexperts.ai>
2025-07-01 16:27:30 -0600

    se configura h2

---
+++ src/main/java/com/tutorial/springboot/entity/Person.java
@@ -0,0 +1,37 @@
+package com.tutorial.springboot.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO person (name, age) VALUES ('Juan', 25);
+INSERT INTO person (name, age) VALUES ('Ana', 30);
+INSERT INTO person (name, age) VALUES ('Flor', 30);
+INSERT INTO person (name, age) VALUES ('Nathan', 38);
+INSERT INTO person (name, age) VALUES ('Esteban', 27);
+INSERT INTO person (name, age) VALUES ('Maria', 20);
+INSERT INTO person (name, age) VALUES ('Marco', 60);
