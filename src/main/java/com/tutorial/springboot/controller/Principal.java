d1bfe3aec06c2632d3acb8481b338f4d7e1a4703
nathan <nathan@cognitiveexperts.ai>
2025-06-30 19:35:23 -0600

    prueba spring actuator

---
+++ src/main/java/com/tutorial/springboot/controller/Principal.java
@@ -0,0 +1,13 @@
+package com.tutorial.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Principal {
+
+    @GetMapping("/hola")
+    public String hola(){
+        return "Hola, haciendo pruebas";
+    }
+}
